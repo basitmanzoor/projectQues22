8a9da5eaf79bcf7c32d519506a0314a5793eb2d7
basitmanzoor <49262108+basitmanzoor@users.noreply.github.com>
2019-04-10 23:32:37 +0530

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -1,18 +1,23 @@
- #include<stdio.h>
+#include<stdio.h>
 int main()
 {
-int p,q,n,a[50];
+int p,q,n,a[60];
 int frame[10];
 int avail, count=0;
-int frame_no, k;
+int frame_no, z;
  
 printf("\n Enter the Number of Pages here :\n");  //here you have to enter the desired number of pages you want
 scanf("%d",&n);
-            printf("\n ENTER THE PAGE NUMBER :\n");
+            printf("\n Enter the Page Numbers here :\n");  //here you need to enter the page numbers
             for(p=1;p<=n;p++)
-            scanf("%d",&a[p]);
-            printf("\n ENTER THE NUMBER OF FRAMES :");
-            scanf("%d",&frame_no);
+             scanf("%d",&a[p]);
+            printf("\n Enter the Number of Frames here :");  //here you need to enter how many frames you need  
+       A:     scanf("%d",&frame_no);
+            if(frame_no<0)
+            {
+            printf("Please enter a positive value\n");
+            goto A;
+        }
 for(p=0;p<frame_no;p++)
             frame[p]= -1;
                         q=0;
@@ -21,16 +26,16 @@ for(p=1;p<=n;p++)
                         {
                                     printf("%d\t\t",a[p]);
                                     avail=0;
-                                    for(k=0;k<frame_no;k++)
-if(frame[k]==a[p])
+                                    for(z=0;z<frame_no;z++)
+if(frame[z]==a[p])
                                                 avail=1;
                                     if (avail==0)
                                     {
                                                 frame[q]=a[p];
                                                 q=(q+1)%frame_no;
                                                 count++;
-                                                for(k=0;k<frame_no;k++)
-                                                printf("%d\t",frame[k]);
+                                                for(z=0;z<frame_no;z++)
+                                                printf("%d\t",frame[z]);
 }
                                     printf("\n");
 }
